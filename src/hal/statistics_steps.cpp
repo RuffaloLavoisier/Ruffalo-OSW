2f105ce23e498c678068d5d7d435be4da80f5a9c
VMsunghwan <jsh970307@daum.net>
2021-12-05 21:29:15 +0900

    update file

--- src/hal/statistics_steps.cpp
+++ src/hal/statistics_steps.cpp
@@ -10,36 +10,43 @@
 void OswHal::setupSteps() {
 #ifdef OSW_FEATURE_STATS_STEPS
   Preferences prefs;
-  prefs.begin(PREFS_STEPS, false); // Open in RW, just in case
-  if(prefs.getBytes(PREFS_STEPS_STATS, &this->_stepsCache, sizeof(this->_stepsCache)) != sizeof(this->_stepsCache)) {
+  prefs.begin(PREFS_STEPS, false);  // Open in RW, just in case
+  if (prefs.getBytes(PREFS_STEPS_STATS, &this->_stepsCache, sizeof(this->_stepsCache)) != sizeof(this->_stepsCache)) {
     // Uoh, the steps history is not initialized -> fill it with zero and do it now!
-    for(size_t i = 0; i < 7; i++)
-      this->_stepsCache[i] = 0;
+    for (size_t i = 0; i < 7; i++) this->_stepsCache[i] = 0;
     prefs.putBytes(PREFS_STEPS_STATS, &this->_stepsCache, sizeof(this->_stepsCache));
-  } else
+  } 
+  else 
+  {
     prefs.getBytes(PREFS_STEPS_STATS, &this->_stepsCache, sizeof(this->_stepsCache));
+  }
   this->_stepsSum = prefs.getUInt(PREFS_STEPS_ALL);
   uint32_t lastDoW = prefs.getUInt(PREFS_STEPS_DAYLASTCHECKED);
-  uint32_t currDoM = 0; // Unused, but required by function signature
+  uint32_t currDoM = 0;  // Unused, but required by function signature
   uint32_t currDoW = 0;
   this->getDate(&currDoM, &currDoW);
-  if(currDoW != lastDoW) {
+  if (currDoW != lastDoW) {
     const uint32_t currentSteps = this->getAccelStepCount();
-    this->_stepsCache[lastDoW] = currentSteps; // write current step to last dow
-    this->_stepsSum += currentSteps; // Let's just hope this never rolls over...
+    this->_stepsCache[lastDoW] = currentSteps;  // write current step to last dow
+    this->_stepsSum += currentSteps;            // Let's just hope this never rolls over...
     this->resetAccelerometer();
     this->initAccelerometer();
-    if(currDoW > lastDoW) {
+    if (currDoW > lastDoW) {
       // set stepscache to 0 in ]lastDoW, currDoW[
-      for(uint32_t i = currDoW; lastDoW + 1 < i; i--)
-        this->_stepsCache[i - 1] = 0;
-    } else {
+      for (uint32_t i = currDoW; lastDoW + 1 < i; i--) this->_stepsCache[i - 1] = 0;
+    } 
+    else 
+    {
       // set > last dow to 0 && set < curr dow to 0
-      if(currDoW > 0)
-        for(uint32_t i = currDoW; 0 < i; i--)
+      if (currDoW > 0) 
+      {
+        for (uint32_t i = currDoW; 0 < i; i--) {
           this->_stepsCache[i - 1] = 0;
-      for(uint32_t i = lastDoW + 1; i < 7; i++)
+        }
+      }
+      for (uint32_t i = lastDoW + 1; i < 7; i++) {
         this->_stepsCache[i] = 0;
+      }
     }
 #ifdef DEBUG
     Serial.println(String(__FILE__) + ": Updated steps from DoW " + String(lastDoW) + " to DoW " + String(currDoW));
@@ -52,28 +59,25 @@ void OswHal::setupSteps() {
   }
   prefs.end();
 #ifdef DEBUG
-    Serial.print(String(__FILE__) + ": Current step history (day " + String(currDoW) + ", sum " + String(this->_stepsSum) + ") is: {");
-    for(size_t i = 0; i < 7; i++) {
-      if(i > 0)
-        Serial.print(", ");
-      Serial.print(this->_stepsCache[i]);
-    }
-    Serial.println("}");
+  Serial.print(String(__FILE__) + ": Current step history (day " + String(currDoW) + ", sum " +
+               String(this->_stepsSum) + ") is: {");
+  for (size_t i = 0; i < 7; i++) {
+    if (i > 0) Serial.print(", ");
+    Serial.print(this->_stepsCache[i]);
+  }
+  Serial.println("}");
 #endif
 #endif
 }
 
-uint32_t OswHal::getStepsToday(void) {
-  return this->getAccelStepCount();
-}
+uint32_t OswHal::getStepsToday(void) { return this->getAccelStepCount(); }
 
 #ifdef OSW_FEATURE_STATS_STEPS
 uint32_t OswHal::getStepsOnDay(uint8_t dayOfWeek) {
   uint32_t day = 0;
   uint32_t weekday = 0;
   getDate(&day, &weekday);
-  if (dayOfWeek == weekday)
-    return this->getStepsToday();
+  if (dayOfWeek == weekday) return this->getStepsToday();
   return this->_stepsCache[dayOfWeek];
 }
 #endif
